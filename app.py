c37852ff3f4e2c361be665ac1855648aa97be38e
heffo42 <davidheff39@gmail.com>
2020-11-17 12:55:10 -0500

    added company search

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask import jsonify
 from pymongo import MongoClient
 from flask import request
+from flask import Response
+import json
 
 
 client = MongoClient('mongodb://david:cis400@157.230.216.187:27017')
@@ -17,6 +19,26 @@ def hello_world():
     search = request.args.get('search')
     print(search)
     return jsonify(list(dev.distinct( "DrugName", {"DrugName" : {'$regex':  f'^{search}.*' }} ))[:10])
+
+@app.route('/company')
+def company_search():
+    search = request.args.get('search')
+    print(search)
+    return jsonify(list(dev.distinct( "CompanyName", {"CompanyName" : {'$regex':  f'^{search}.*' }} ))[:10])
+
+
+@app.route('/details')
+def get_drug_details():
+    drug_name = request.args.get('drug_name')
+    print(f'drug name {drug_name}')
+    x = list(dev.find({'DrugName': drug_name},  {'_id': 0}))
+    return Response(json.dumps(x),  mimetype='application/json')
+
+@app.route('/coco')
+def coco():
+    return jsonify('hi coco')
+
+
 
 
 print('made it here')
